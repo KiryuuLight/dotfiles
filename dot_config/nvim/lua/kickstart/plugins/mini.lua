93d4c18a8fc89f074f72f523187adf1cb066eeab
Luis Ortiz <70501251+KiryuuLight@users.noreply.github.com>
2025-04-06 19:29:38 -0500

    chore: better tag selection , class , fn and loop selections

--- dot_config/nvim/lua/kickstart/plugins/mini.lua
+++ dot_config/nvim/lua/kickstart/plugins/mini.lua
@@ -2,8 +2,19 @@ return {
   {
     'echasnovski/mini.nvim',
     config = function()
-      require('mini.ai').setup { n_lines = 500 }
+      local spec_treesitter = require('mini.ai').gen_spec.treesitter
+      require('mini.ai').setup {
+        n_lines = 500,
+        custom_textobjects = {
+          t = { '<([%p%w]-)%f[^<%w][^<>]->.-</%1>', '^<.->().*()</[^/]->$' },
+          o = spec_treesitter {
+            a = { '@block.outer', '@conditional.outer', '@loop.outer' },
+            i = { '@block.inner', '@conditional.inner', '@loop.inner' },
+          },
+          f = spec_treesitter { a = '@function.outer', i = '@function.inner' },
+          c = spec_treesitter { a = '@class.outer', i = '@class.inner' },
+        },
+      }
     end,
   },
 }
--- vim: ts=2 sts=2 sw=2 et
