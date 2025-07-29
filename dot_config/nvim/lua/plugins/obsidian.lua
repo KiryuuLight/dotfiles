adfc19ddcba7a3f3ce45d2de47784a1666d8a60b
Luis Ortiz <70501251+KiryuuLight@users.noreply.github.com>
2025-07-28 21:33:28 -0500

    feat[obsidian]: carryover tasks from yesterday & only one workspace

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -6,22 +6,8 @@ return {
     'nvim-lua/plenary.nvim',
   },
   opts = {
-    workspaces = {
-      {
-        name = 'personal',
-        path = '~/vaults/personal',
-        overrides = {
-          notes_subdir = 'notes',
-        },
-      },
-      {
-        name = 'work',
-        path = '~/vaults/work',
-        overrides = {
-          notes_subdir = 'notes',
-        },
-      },
-    },
+    dir = '~/vaults/personal',
+    notes_subdir = 'notes',
     mappings = {
       ['gf'] = {
         action = function()
@@ -42,22 +28,59 @@ return {
       folder = 'templates',
       date_format = '%Y-%m-%d-%a',
       time_format = '%H:%M',
+      substitutions = {
+        tasks_from_yesterday = function()
+          local yesterday = os.date('%Y-%m-%d', os.time() - 24 * 60 * 60)
+          local vault_path = vim.fn.expand '~/vaults/personal/notes/dailies'
+          local yesterday_note_path = string.format('%s/%s.md', vault_path, yesterday)
+
+          if vim.fn.filereadable(yesterday_note_path) == 0 then
+            return 'No notes from yesterday'
+          end
+
+          local lines = vim.fn.readfile(yesterday_note_path)
+
+          local tasks = {}
+          local current_task = nil
+
+          for _, line in ipairs(lines) do
+            if line:match '^%s*%- %[ %]' then
+              -- Start a new task
+              if current_task then
+                table.insert(tasks, table.concat(current_task, '\n'))
+              end
+              current_task = { line }
+            elseif current_task and line:match '^%s+' then
+              -- Continuation of previous task
+              table.insert(current_task, line)
+            else
+              -- End of a task block
+              if current_task then
+                table.insert(tasks, table.concat(current_task, '\n'))
+                current_task = nil
+              end
+            end
+          end
+
+          -- Catch the last task block
+          if current_task then
+            table.insert(tasks, table.concat(current_task, '\n'))
+          end
+
+          return #tasks > 0 and table.concat(tasks, '\n') or 'No unchecked tasks from yesterday.'
+        end,
+      },
     },
     ui = {
-      enable = false, -- set to false to disable all additional syntax features
+      enable = false,
     },
     ---@param title string|?
     ---@return string
     note_id_func = function(title)
-      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-      -- In this case a note with the title 'My new note' will be given an ID that looks
-      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
       local suffix = ''
       if title ~= nil then
-        -- If title is given, transform it into valid file name.
         suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
       else
-        -- If title is nil, just add 4 random uppercase letters to the suffix.
         for _ = 1, 4 do
           suffix = suffix .. string.char(math.random(65, 90))
         end
